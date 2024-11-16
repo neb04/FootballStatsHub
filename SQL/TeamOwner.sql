959067a048efb9572965183d6ec9dbcb6f847215
neb04 <pododog24@gmail.com>
2024-11-15 19:15:55 -0600

    moved sql files

---
+++ SQL/TeamOwner.sql
@@ -0,0 +1,33 @@
+-- Insert statements for all 32 NFL team owners
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (1, 'Arthur', 'Blank'); -- Falcons
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (2, 'Terry', 'Pegula'); -- Bills
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (3, 'Virginia', 'McCaskey'); -- Bears
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (4, 'Mike', 'Brown'); -- Bengals
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (5, 'Jimmy', 'Haslam'); -- Browns
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (6, 'Jerry', 'Jones'); -- Cowboys
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (7, 'Rob', 'Walton'); -- Broncos
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (8, 'Sheila', 'Ford'); -- Lions
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (9, 'Mark', 'Murphy'); -- Packers
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (10, 'Amy', 'Adams Strunk'); -- Titans
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (11, 'Jim', 'Irsay'); -- Colts
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (12, 'Clark', 'Hunt'); -- Chiefs
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (13, 'Mark', 'Davis'); -- Raiders
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (14, 'Stan', 'Kroenke'); -- Rams
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (15, 'Stephen', 'Ross'); -- Dolphins
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (16, 'Zygi', 'Wilf'); -- Vikings
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (17, 'Robert', 'Kraft'); -- Patriots
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (18, 'Gayle', 'Benson'); -- Saints
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (19, 'John', 'Mara'); -- Giants
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (20, 'Woody', 'Johnson'); -- Jets
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (21, 'Jeffrey', 'Lurie'); -- Eagles
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (22, 'Michael', 'Bidwill'); -- Cardinals
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (23, 'Art', 'Rooney II'); -- Steelers
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (24, 'Dean', 'Spanos'); -- Chargers
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (25, 'Jed', 'York'); -- 49ers
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (26, 'Jody', 'Allen'); -- Seahawks
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (27, 'Bryan', 'Glazer'); -- Buccaneers
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (28, 'Josh', 'Harris'); -- Commanders
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (29, 'David', 'Tepper'); -- Panthers
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (30, 'Shahid', 'Khan'); -- Jaguars
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (31, 'Steve', 'Bisciotti'); -- Ravens
+INSERT INTO TeamOwner (ownerID, f_Name, l_Name) VALUES (32, 'Janice', 'McNair'); -- Texans
